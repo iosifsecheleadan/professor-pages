1a23711485a93d6b9947d7ed467f998104201e09
iosifsecheleadan <iosifsecheleadan@protonmail.com>
2021-01-23 12:09:20 +0200

    Revert "fixed import"
    This reverts commit d3586e60634e7d245d7fea331721ad0d249732ad.

--- src/repo/course_week.py
+++ src/repo/course_week.py
@@ -1,5 +1,4 @@
 from src.domain.data.course_week import CourseWeek
-from src.domain.data.factory import DataFactory
 from src.repo.repository import FileRepositoryInterface
 from os.path import normpath
 
